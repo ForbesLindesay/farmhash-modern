13ff08613d33b4aa64fdf14ed2b9ff6e1cecadb1
Forbes Lindesay <forbes@lindesay.co.uk>
2023-06-29 14:59:37 +0100

    ci: fix publish build (#3)

--- rust-src/src/lib.rs
+++ rust-src/src/lib.rs
@@ -3,12 +3,6 @@ mod utils;
 use std::mem;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn fingerprint32(input: &[u8]) -> u32 {
     utils::set_panic_hook();
